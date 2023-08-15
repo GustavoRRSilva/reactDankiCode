905c7668c4081acfef60b4ca55dbdca8da4bcd24
Gustavo <gstvrrds@gmail.com>
2023-08-15 10:14:17 -0300

    fim da aula renderização condicional

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
 import "./App.css";
 import Menu from "./menu";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function App(props) {
-  const [count,setCount] = useState(0); //count é o valor inicial e sempre que for trocar tem que usar o setCount
-  useEffect(() =>{
-    document.title =`você clicou ${count} vezes`
-  },[count]);//efeito usado p toda vez q a pagina atualiza, e com o ultimo parametro servindo para atualizar toda vez q o count mudar
-  return (
-    <div className="App">
-      <Menu />
-      <p>Você clicou {count} vezes</p>
-      <button onClick={() => setCount(count + 1)}>Click me </button>
-    </div>
-  );
+  const [count, setCount] = useState(0); //count é o valor inicial e sempre que for trocar tem que usar o setCount
+  useEffect(() => {
+    document.title = `você clicou ${count} vezes`;
+  }, [count]); //efeito usado p toda vez q a pagina atualiza, e com o ultimo parametro servindo para atualizar toda vez q o count mudar
+  const [nome, setNome] = useState("Gustavo");
+  if ((nome == "Gustavo")) {
+    return (
+      <div className="App">
+        <Menu />
+        <p>Seu nome é gustavo</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <Menu />
+        <p>Seu nome não é gustavo</p>
+      </div>
+    );
+  }
 }
-
 export default App;
