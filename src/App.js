706db78bd333a3ccb4e1b3121a130b56d2d276dc
Gustavo <gstvrrds@gmail.com>
2023-08-15 10:10:30 -0300

    fim da aula React hooks de efeito

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 import Menu from "./menu";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 function App(props) {
   const [count,setCount] = useState(0); //count é o valor inicial e sempre que for trocar tem que usar o setCount
+  useEffect(() =>{
+    document.title =`você clicou ${count} vezes`
+  },[count]);//efeito usado p toda vez q a pagina atualiza, e com o ultimo parametro servindo para atualizar toda vez q o count mudar
   return (
     <div className="App">
       <Menu />
