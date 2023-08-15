5ddda3281eae1634e0d9a744b4a2bc510f4bc223
Gustavo <gstvrrds@gmail.com>
2023-08-15 10:01:52 -0300

    fim da aula React hooks de estado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import './App.css';
-import Menu from './menu';
+import React from "react";
+import "./App.css";
+import Menu from "./menu";
+import { useState } from "react";
 
 function App(props) {
+  const [count,setCount] = useState(0); //count é o valor inicial e sempre que for trocar tem que usar o setCount
   return (
-   <div className='App'>
-   <h1>
-    {props.texto.chamada}
-   </h1>
-   <p>
-    {props.texto.chamada2}
-   </p>
-   <Menu/>
-   </div>
+    <div className="App">
+      <Menu />
+      <p>Você clicou {count} vezes</p>
+      <button onClick={() => setCount(count + 1)}>Click me </button>
+    </div>
   );
 }
 
